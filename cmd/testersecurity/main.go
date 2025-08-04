7f8d7e41f29be38e01d7afa0c256e1a5277afe65
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 23:22:15 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/testersecurity/main.go
@@ -0,0 +1,23 @@
+// cmd/testersecurity/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"testersecurity/internal/testersecurity"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := testersecurity.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
